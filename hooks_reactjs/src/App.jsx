37acfa16bd3fa0e022af0c2e89e27c3c5f59de51
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-01-15 23:50:16 +0800

    Added: update ARRAY in state

--- hooks_reactjs/src/App.jsx
+++ hooks_reactjs/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 // import MyComponent from './concepts/onchange/MyComponent.jsx'
 import Counter from './concepts/state/Counter.jsx'
 // import ColorPicker from './concepts/colorpicker/ColorPicker';
-import MyComponent from './concepts/updateObject/MyComponent.jsx'
-
+// import MyComponent from './concepts/updateObjectinState/MyComponent.jsx'
+import MyComponent from './concepts/updateArrayinState/MyComponent.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +14,7 @@ function App() {
       {/* < Counter /> */}
       {/* <MyComponent/> */}
       {/* <ColorPicker /> */}
+      {/* <MyComponent/> */}
       <MyComponent/>
     </>
   )
